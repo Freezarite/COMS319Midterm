5bdc0c0361e9730c1b169b6ea554671581c9f2c0
Markus <markus.roth2001@yahoo.de>
2024-03-19 15:48:49 -0500

    created contact-page and added links to header.
    Further changes will be the auto generation of the
    body via js and json files

---
+++ jsFiles/contact.js
@@ -0,0 +1,13 @@
+const icons = new Map();
+
+//puting icon html code into the map
+icons.set('linkedin', '<i class="fa-brands fa-linkedin"></i>')
+icons.set('github', '<i class="fa-brands fa-github-alt"></i>')
+icons.set('intagram', '<i class="fa-brands fa-instagram"></i>')
+icons.set('twitter', '<i class="fa-brands fa-square-x-twitter"></i>')
+icons.set('phone', '<i class="fa-solid fa-phone"></i>')
+icons.set('mail', '<i class="fa-regular fa-envelope"></i>')
+icons.set('facebook', '<i class="fa-brands fa-facebook"></i>')
+icons.set('discord', '<i class="fa-brands fa-discord"></i>')
+icons.set('youtube', '<i class="fa-brands fa-youtube"></i>')
+
